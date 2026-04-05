0d4bfa299321cab708c2fa93d2781ea916ec0235
Ting-Wei Chin <jordan20130315@gmail.com>
2026-04-05 18:44:45 +0800

    fix: use cors package for proper CORS headers

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const db = require('./db');
 
 // CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL || '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static('public'));
